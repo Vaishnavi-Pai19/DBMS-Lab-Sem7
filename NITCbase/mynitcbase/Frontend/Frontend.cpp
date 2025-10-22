dbe6e7530d5abefb69e3f5298d140f49c686e83f
vaish1905 <vaish1905@localhost.localdomain>
2025-10-23 01:01:21 +0530

    Stage 12 last exercise left

--- NITCbase/mynitcbase/Frontend/Frontend.cpp
+++ NITCbase/mynitcbase/Frontend/Frontend.cpp
@@ -6,33 +6,27 @@
 int Frontend::create_table(char relname[ATTR_SIZE], int no_attrs, char attributes[][ATTR_SIZE],
                            int type_attrs[]) {
   return Schema::createRel(relname, no_attrs, attributes, type_attrs);
-  return SUCCESS;
 }
 
 int Frontend::drop_table(char relname[ATTR_SIZE]) {
   return Schema::deleteRel(relname);
-  return SUCCESS;
 }
 
 int Frontend::open_table(char relname[ATTR_SIZE]) {
   return Schema::openRel(relname);
-  return SUCCESS;
 }
 
 int Frontend::close_table(char relname[ATTR_SIZE]) {
   return Schema::closeRel(relname);
-  return SUCCESS;
 }
 
 int Frontend::alter_table_rename(char relname_from[ATTR_SIZE], char relname_to[ATTR_SIZE]) {
   return Schema::renameRel(relname_from, relname_to);
-  return SUCCESS;
 }
 
 int Frontend::alter_table_rename_column(char relname[ATTR_SIZE], char attrname_from[ATTR_SIZE],
                                         char attrname_to[ATTR_SIZE]) {
   return Schema::renameAttr(relname, attrname_from, attrname_to);
-  return SUCCESS;
 }
 
 int Frontend::create_index(char relname[ATTR_SIZE], char attrname[ATTR_SIZE]) {
@@ -45,7 +39,6 @@ int Frontend::drop_index(char relname[ATTR_SIZE], char attrname[ATTR_SIZE]) {
 
 int Frontend::insert_into_table_values(char relname[ATTR_SIZE], int attr_count, char attr_values[][ATTR_SIZE]) {
   return Algebra::insert(relname, attr_count, attr_values);
-  return SUCCESS;
 }
 
 // SELECT * FROM RelName INTO TargetName
@@ -71,19 +64,19 @@ int Frontend::select_from_table_where(char relname_source[ATTR_SIZE], char relna
 int Frontend::select_attrlist_from_table_where(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
                                                int attr_count, char attr_list[][ATTR_SIZE],
                                                char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
-  // Algebra::select + Algebra::project??
+  // Algebra::select + Algebra::project
   
   // Creating a temporary target relation with name ".temp" (constant TEMP)
   // TEMP will contain all the attributes of the source relation 
   char tempStr[] = TEMP;
   int response = Algebra::select(relname_source, tempStr, attribute, op, value);
 
-  if(response != SUCCESS) {
+  if (response != SUCCESS) {
     return response;
   }
 
   int tempRelId = OpenRelTable::openRel(tempStr);
-  if(tempRelId < 0) {
+  if (tempRelId < 0) {
     Schema::deleteRel(tempStr);
     return tempRelId;
   }
@@ -98,16 +91,31 @@ int Frontend::select_attrlist_from_table_where(char relname_source[ATTR_SIZE], c
 int Frontend::select_from_join_where(char relname_source_one[ATTR_SIZE], char relname_source_two[ATTR_SIZE],
                                      char relname_target[ATTR_SIZE],
                                      char join_attr_one[ATTR_SIZE], char join_attr_two[ATTR_SIZE]) {
-  // Algebra::join
-  return SUCCESS;
+  return Algebra::join(relname_source_one, relname_source_two, relname_target, join_attr_one, join_attr_two);
 }
 
 int Frontend::select_attrlist_from_join_where(char relname_source_one[ATTR_SIZE], char relname_source_two[ATTR_SIZE],
                                               char relname_target[ATTR_SIZE],
                                               char join_attr_one[ATTR_SIZE], char join_attr_two[ATTR_SIZE],
                                               int attr_count, char attr_list[][ATTR_SIZE]) {
-  // Algebra::join + project
-  return SUCCESS;
+  char tempStr[] = TEMP;
+  int response = Algebra::join(relname_source_one, relname_source_two, tempStr, join_attr_one, join_attr_two);
+
+  if (response != SUCCESS) {
+    return response;
+  }
+
+  int tempRelId = OpenRelTable::openRel(tempStr);
+  if (tempRelId < 0) {
+    Schema::deleteRel(tempStr);
+    return tempRelId;
+  }
+
+  response = Algebra::project(tempStr, relname_target, attr_count, attr_list);
+
+  OpenRelTable::closeRel(tempRelId);
+  Schema::deleteRel(tempStr);
+  return response;
 }
 
 int Frontend::custom_function(int argc, char argv[][ATTR_SIZE]) {
